229a6c880e71a6c37cdbee940618614253aa4c01
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 12:47:24 +0200

    rename file for BusEvent baseclass to bus-event.base.ts

--- mod.ts
+++ mod.ts
@@ -4,4 +4,4 @@
  */
 // Event Bus Section with basic BusEvent<> generic type
 export * from "/lib/event-bus/event-bus.ts";
-export * from "/lib/event-bus/bus-event.type.ts";
+export * from "/lib/event-bus/bus-event.base.ts";
