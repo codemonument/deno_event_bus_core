e464c042dede217fa86df05fe6401af43907f1e2
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 11:36:23 +0200

    ad the dnt-build script for building this lib as npm package

---
+++ scripts/dnt-build.ts
@@ -0,0 +1,33 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt@0.23.0/mod.ts";
+import { VERSION } from "/version.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    // TODO: package name is not final!
+    name: "@codemonument/event-bus-core",
+    version: VERSION,
+    description: "A typescript event bus with fully typed events",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/codemonument/event-bus-core.git",
+    },
+    bugs: {
+      url: "https://github.com/codemonument/event-bus-core/issues",
+    },
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
