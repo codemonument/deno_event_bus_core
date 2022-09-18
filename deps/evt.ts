6d23cd56de7966537cec6314fe8df498978cedc9
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 18:35:07 +0200

    change evt dependency to version from skypack

--- deps/evt.ts
+++ deps/evt.ts
@@ -1,1 +1,8 @@
-export * from "https://deno.land/x/evt@v2.4.2/mod.ts"
+/**
+ * bje- 2022-09-18: Problem with deno.land import of evt,
+ * bc. the type autocomplete does not work for some reason.
+ */
+
+// export * from "https://deno.land/x/evt@v2.4.2/mod.ts"
+
+export * from "https://cdn.skypack.dev/evt@v2.4.2?dts";
