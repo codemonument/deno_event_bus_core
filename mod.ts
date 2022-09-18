a26bc8a2227ea5346e619de35cf68fecd8cdde7f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 13:48:37 +0200

    change module exports in mod.ts to relative paths

--- mod.ts
+++ mod.ts
@@ -3,5 +3,5 @@
  * which should be used by other people
  */
 // Event Bus Section with basic BusEvent<> generic type
-export * from "/lib/event-bus/event-bus.ts";
-export * from "/lib/event-bus/bus-event.base.ts";
+export * from "./lib/event-bus/event-bus.ts";
+export * from "./lib/event-bus/bus-event.base.ts";
