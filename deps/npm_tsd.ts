9d58b3125d803709ba7a081a2c49812a684d57af
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 13:16:21 +0200

    update tsd import

--- deps/npm_tsd.ts
+++ deps/npm_tsd.ts
@@ -1,1 +1,4 @@
-export * from "npm:tsd@0.24.1";
+// export * from "npm:tsd@0.22.0";
+// export * from "npm:tsd@0.24.1/dist/index.ts";
+// export * from "https://cdn.skypack.dev/tsd@0.24.1?dts";
+export * from "https://cdn.skypack.dev/-/tsd@v0.24.1-Zs6K9vV62dJrKk0WNWvh/dist=es2019,mode=imports/optimized/tsd.js";
