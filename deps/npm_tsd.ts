835a39fd759795d57eba0a832c8af4a36b1ec839
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 14:17:09 +0200

    revert npm_tsd dep to skypack

--- deps/npm_tsd.ts
+++ deps/npm_tsd.ts
@@ -3,7 +3,7 @@
 
 // TODO: When working:  Replace direct import in bus-event.base.test.ts with 'tsd' bare import via importMap
 
-export * from "npm:tsd@0.24.1";
+// export * from "npm:tsd@0.24.1";
 
-// export * from "https://cdn.skypack.dev/tsd@0.24.1?dts";
-// export * from "https://cdn.skypack.dev/-/tsd@v0.24.1-Zs6K9vV62dJrKk0WNWvh/dist=es2019,mode=imports/optimized/tsd.js";
+// DO NOT RESOLVE THE REDIRECT! (since the url get's more ugly & not useful)
+export * from "https://cdn.skypack.dev/tsd@0.24.1?dts";
