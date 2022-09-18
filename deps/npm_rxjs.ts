765cbc7b9c793eac1a6f96dcd40b906d5b0470c1
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 14:02:00 +0200

    add todo in npm_rxjs dependency file

--- deps/npm_rxjs.ts
+++ deps/npm_rxjs.ts
@@ -1,1 +1,6 @@
+// Note: Re-Exporting npm: specifiers seems not to work in deno 1.25.3 yet.
+// TS complains about non-exported members which i know are there
+
+// TODO: Replace direct import in event-bus.ts with 'rxjs' bare import via importMap
+
 export * from "npm:rxjs@7.5.6";
