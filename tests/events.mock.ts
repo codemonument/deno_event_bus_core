a4b41eb63a7a1ecf3892a8737291a2a9eeb1f763
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-09-18 12:48:10 +0200

    add a file with mock events for testing

---
+++ tests/events.mock.ts
@@ -0,0 +1,19 @@
+import { BusEvent } from "/mod.ts";
+
+/**
+ * For testing events without payload
+ */
+export class PlainEvent extends BusEvent<void> {
+  public type = "PlainEvent";
+}
+
+/**
+ * For testing events with payload
+ */
+export interface DemoPayload {
+  name: string;
+}
+
+export class EventWithPayload extends BusEvent<DemoPayload> {
+  public type = "EventWithPayload";
+}
