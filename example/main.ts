baf0d19afe2bda6cee692a1e3ba53b31722dc5e9
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-14 20:28:40 +0100

    add exmaple code

--- example/main.ts
+++ example/main.ts
@@ -1,10 +1,25 @@
-import {} from "/mod.ts";
 import { log } from "std_log";
-import { VERSION } from "/version.ts";
+import { EventBus } from "../lib/event-bus/event-bus.rxjs.ts";
+import { BusEvent } from "@/mod.ts";
 
-try {
-  log.info(`Module Version (version.ts): ${VERSION}`);
-} catch (error) {
-  console.error(error);
-  Deno.exit();
+const bus = new EventBus();
+
+export class PauseEvent extends BusEvent<void> {
+  public type = "PauseEvent";
 }
+
+export class SetCellValue extends BusEvent<{ value: number }> {
+  public type = "SetCellValue";
+}
+
+bus.on$(PauseEvent).subscribe(() => {
+  console.log(`Pause called!`);
+});
+
+bus.on$(SetCellValue).subscribe((payload: any) => {
+  console.log(`Set Cell Value called!`, payload);
+});
+
+bus.emit(new PauseEvent());
+
+bus.emit(new SetCellValue({ value: 10 }));
