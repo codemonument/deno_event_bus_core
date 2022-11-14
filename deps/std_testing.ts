fb8220fd2271c25bd3d4a750522fe82f5f7c61a8
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-14 22:24:14 +0100

    upgrade deno std lib

--- deps/std_testing.ts
+++ deps/std_testing.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.156.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.156.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.156.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.164.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.164.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.164.0/testing/snapshot.ts";
