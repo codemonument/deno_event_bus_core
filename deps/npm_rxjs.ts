8deb53e963fab97c24122bf1d82132382a692541
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-14 22:52:04 +0100

    switch from npm:rxjs@7.5.7 to esm.sh import

--- deps/npm_rxjs.ts
+++ deps/npm_rxjs.ts
@@ -1,7 +1,6 @@
-// Note: Re-Exporting npm: specifiers seems not to work in deno 1.25.3 yet.
-// TS complains about non-exported members which i know are there
-
-// TODO: When working: Replace direct import in event-bus.ts with 'rxjs' bare import via importMap
-
-export * from "npm:rxjs@7.5.7";
+// Note: Compiling with dnt does not allow npm: imports, tested with version dnt@0.31.0
+// export * from "npm:rxjs@7.5.7";
 // export * from "https://cdn.skypack.dev/rxjs@7.5.7?dts";
+
+// use esm.sh bc. it also has full type autocomplete for rxjs!
+export * from "https://esm.sh/rxjs@7.5.7";
