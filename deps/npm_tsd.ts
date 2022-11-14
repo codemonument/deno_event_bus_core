b2ac3a87844e8264317b4bed8b2f7a34d77d0553
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-14 22:44:23 +0100

    switch from tsd on skypack to the npm: import

--- deps/npm_tsd.ts
+++ deps/npm_tsd.ts
@@ -1,9 +1,7 @@
 // Note: Re-Exporting npm: specifiers seems not to work in deno 1.25.3 yet.
 // TS complains about non-exported members which i know are there
 
-// TODO: When working:  Replace direct import in bus-event.base.test.ts with 'tsd' bare import via importMap
-
-// export * from "npm:tsd@0.24.1";
+export * from "npm:tsd@0.24.1";
 
 // DO NOT RESOLVE THE REDIRECT! (since the url get's more ugly & not useful)
-export * from "https://cdn.skypack.dev/tsd@0.24.1?dts";
+// export * from "https://cdn.skypack.dev/tsd@0.24.1?dts";
