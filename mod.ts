0ad993ea5890c1d925b8b7f5a113eb216eab0058
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-16 00:40:50 +0100

    reformat mod.ts

--- mod.ts
+++ mod.ts
@@ -2,13 +2,16 @@
  * Export all functionality of your module here,
  * which should be used by other people
  */
+
 // Event Bus Section with basic BusEvent<> generic type
 export * from "./lib/event-bus/event-bus.rxjs.ts";
+export * from "./lib/event-bus/bus-event.base.ts";
+
+// Event-Bus-Group Section
 export * from "./lib/event-bus-group/event-bus-group.ts";
+export * from "./lib/event-bus-group/event-group-callback.type.ts";
 
 /**
  * CAUTION: evt event bus Heavily work in progress!
  */
 // export * from "./lib/event-bus/event-bus.evt.ts";
-
-export * from "./lib/event-bus/bus-event.base.ts";
