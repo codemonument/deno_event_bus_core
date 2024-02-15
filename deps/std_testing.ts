ff95c6e685e1a20c6c826a67034a3190494ea859
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-02-15 03:52:11 +0100

    upgrade std lib to 0.215.0

--- deps/std_testing.ts
+++ deps/std_testing.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/std@0.164.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.164.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.164.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.215.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.215.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.215.0/testing/snapshot.ts";
