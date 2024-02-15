eee9ed744f6e84354c056cff87284b925f2aa23e
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-02-15 03:50:53 +0100

    upgrade npm_tsd

--- deps/npm_tsd.ts
+++ deps/npm_tsd.ts
@@ -1,7 +1,7 @@
 // Note: Re-Exporting npm: specifiers seems not to work in deno 1.25.3 yet.
 // TS complains about non-exported members which i know are there
 
-export * from "npm:tsd@0.24.1";
+export * from "npm:tsd@0.30.4";
 
 // DO NOT RESOLVE THE REDIRECT! (since the url get's more ugly & not useful)
 // export * from "https://cdn.skypack.dev/tsd@0.24.1?dts";
