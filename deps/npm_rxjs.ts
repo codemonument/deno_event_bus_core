6e1a37a7265173b50a23eb70798b67966656dec1
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-07-27 08:33:32 +0200

    update rxjs import to use npm:

--- deps/npm_rxjs.ts
+++ deps/npm_rxjs.ts
@@ -1,6 +1,1 @@
-// Note: Compiling with dnt does not allow npm: imports, tested with version dnt@0.31.0
-// export * from "npm:rxjs@7.5.7";
-// export * from "https://cdn.skypack.dev/rxjs@7.5.7?dts";
-
-// use esm.sh bc. it also has full type autocomplete for rxjs!
-export * from "https://esm.sh/rxjs@7.8.1";
+export * from "npm:rxjs@7.8.1";
